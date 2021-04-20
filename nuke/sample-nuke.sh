7cfe1cae374593ca74567ed78790e5853b10c502
Julio Faerman <jfaerman@gmail.com>
2021-04-20 21:25:35 +0200

    Nuker

--- nuke/sample-nuke.sh
+++ nuke/sample-nuke.sh
@@ -1,4 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR
-./aws-nuke -c ./sample-nuke.yaml --profile sandbox --no-dry-run --force
+./aws-nuke -c ./sample-nuke.yaml --profile sandbox --no-dry-run --force  --force-sleep 3
 popd
