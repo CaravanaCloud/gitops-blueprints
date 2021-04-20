6746e4f08fd66ede0fdbdb6b26e1fbd99ceab1b6
Julio Faerman <jfaerman@gmail.com>
2021-04-20 21:09:06 +0200

    Linted

--- eks/apply.sh
+++ eks/apply.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#
-
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
--- nuke/sample-nuke.sh
+++ nuke/sample-nuke.sh
@@ -1,1 +1,4 @@
-./aws-nuke -c ./sample-nuke.yml --profile sandbox
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd $DIR
+./aws-nuke -c ./sample-nuke.yaml --profile sandbox --no-dry-run --force
+popd
