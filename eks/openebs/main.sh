93b1017091fd86a83819260edc4e1f295c172e80
Julio Faerman <jfaerman@gmail.com>
2021-04-20 19:37:51 +0200

    jq

--- eks/openebs/main.sh
+++ eks/openebs/main.sh
@@ -16,5 +16,16 @@ helm install openebs openebs/openebs
 echo "check helm chart"
 helm ls
 
+echo "verify openebs"
+kubectl get pods -n openebs
+
+echo "setup for cStor Pool / Postgres"
+# https://docs.openebs.io/docs/next/postgres.html
+
+echo "list block devices"
+kubectl get blockdevice -n openebs
+
+
+
 echo "Openebs setup done"
 
